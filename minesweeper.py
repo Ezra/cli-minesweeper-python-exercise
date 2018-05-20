5bfb8203f78c4f579ea848460167d1660bc8e737
Ezra Bradford <ezra.bradford@gmail.com>
2018-05-20 12:25:16 -0700

    now runnable

--- minesweeper.py
+++ minesweeper.py
@@ -1,2 +1,9 @@
 from __future__ import print_function, division, unicode_literals, absolute_import
 
+import sys
+
+def main(argv=None):
+    print('nothing in particular')
+
+if __name__ == '__main__':
+    main(sys.argv)
