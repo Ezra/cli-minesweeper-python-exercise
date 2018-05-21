406b800a72ea7f82242333b0644b2dd028cf76b1
Ezra Bradford <ezra.bradford@gmail.com>
2018-05-20 17:35:48 -0700

    demonstrate very basic console IO

--- minesweeper.py
+++ minesweeper.py
@@ -2,8 +2,12 @@ from __future__ import print_function, division, unicode_literals, absolute_impo
 
 import sys
 
+
 def main(argv=None):
     print('nothing in particular')
+    words = str(raw_input('type something > '))
+    print('you typed {}'.format(words))
+
 
 if __name__ == '__main__':
     main(sys.argv)
