55021bd77680b70c94f59b595161b9308c5404ed
Ezra Bradford <ezra.bradford@gmail.com>
2018-05-20 18:38:49 -0700

    use cmd library for more advanced IO

--- minesweeper.py
+++ minesweeper.py
@@ -1,12 +1,28 @@
 from __future__ import print_function, division, unicode_literals, absolute_import
 
 import sys
+from cmd import Cmd
+
+
+class MinesweeperCmd(Cmd):
+    """Handle commands for a game of Minesweeper."""
+
+    def do_exit(self, _):
+        """Exit the game."""
+        return True
+
+    def do_quit(self, text):
+        """Exit the game. (Synonym for exit.)"""
+        return self.do_exit(text)
+
+    def do_EOF(self, text):
+        """Exit the game. (Synonym for exit.)"""
+        return self.do_exit(text)
 
 
 def main(argv=None):
-    print('nothing in particular')
-    words = str(raw_input('type something > '))
-    print('you typed {}'.format(words))
+    interpreter = MinesweeperCmd()
+    interpreter.cmdloop()
 
 
 if __name__ == '__main__':
