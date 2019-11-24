a7705febc8160642770799a2f6c11223edbf698a
Ezra Bradford <ezra.bradford@gmail.com>
2019-11-23 19:15:20 -0800

    add more tests (29% -> 51%)

--- tests/test_MinesweeperBoard.py
+++ tests/test_MinesweeperBoard.py
@@ -1,6 +1,20 @@
-import MinesweeperBoard
+from MinesweeperBoard import KnowledgeState, EndState, MinesweeperBoard
 
 
 def test_knowledge_state():
-    state = MinesweeperBoard.KnowledgeState.FLAGGED
+    state = KnowledgeState.FLAGGED
     assert str(state) == '#'
+
+
+def test_init():
+    board = MinesweeperBoard(3, 3, 1)
+    assert(not board.end_state)
+
+
+def test_victory():
+    board = MinesweeperBoard(2, 1, 1)
+    assert(not board.end_state)
+    board.step(0, 0)
+    assert(board.end_state == EndState.VICTORY)
+    board.step(1, 0)
+    assert(board.end_state == EndState.VICTORY)
